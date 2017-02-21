6cce97939d3ce57476aa0a969e4e5384f8d38a77
charles.liao <charleslxh@icloud.com>
2017-02-22 00:07:24 +0800

    init repertory

---
+++ src/cdc/index.js
@@ -0,0 +1,13 @@
+/**
+*                                       Content Delivery Center
+* Handle PUB/SUB.
+* Forward or broadcast content.
+**/
+
+var event = require('event');
+
+class CDC extends event.Emitter {
+
+}
+
+module.exports = CDC
---
+++ src/server.js
@@ -0,0 +1,12 @@
+var event = require('event');
+var cbc = require('./cdc')
+
+class Server extends event.eventEmitter {
+  constructor(options) {
+    super()
+  }
+
+  _init() {
+
+  }
+}
